75441f0f9935c69f36896c93bc14cc1f179aa58d
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2025-07-19 04:39:52 -0600

    +read_vectors

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 // Deriving Ord allows for direct sorting. The order is important: word1 then word2.
 #[derive(Debug, Copy, Clone, Default)]
@@ -33,6 +37,32 @@ impl PartialEq for Crec {
 }
 
 impl Eq for Crec {} // Marker trait - necessary for Ord
+
+// read vectors and normalise to unit norms
+pub fn read_vectors(filename: &str) -> Result<HashMap<String, Vec<f64>>, Box<dyn Error>> {
+    const EPS: f64 = 1e-8;
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+    let mut vectors = HashMap::new();
+
+    for line_result in reader.lines() {
+        let line = line_result?;
+        let mut parts = line.trim().split_whitespace();
+
+        if let Some(key) = parts.next() {
+            let mut values = parts
+                .map(|s| s.parse::<f64>())
+                .collect::<Result<Vec<f64>, _>>()?;
+            let norm: f64 = values.iter().map(|x| x * x).sum::<f64>().sqrt();
+            if norm > EPS {
+                values.iter_mut().for_each(|e| *e /= norm);
+            }
+            vectors.insert(key.to_string(), values);
+        }
+    }
+
+    Ok(vectors)
+}
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
